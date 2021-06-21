f08175bc9c8f4a0d6dbfb3e2532088f3622dc25b
Brandon Ingli <brandon@brandoningli.com>
2021-06-21 01:23:58 -0500

    Revert "Restrict SpikeKit Features"
    This reverts commit a5781a3f4523b1db3fbd5dfa67431d861b03c2d1.

--- src/samplePlugin/main.js
+++ src/samplePlugin/main.js
@@ -40,9 +40,10 @@ function help(command, args) {
  * Handles incoming commands for this plugin.
  * @param {string} command The command issued, without the prefix.
  * @param {string} args The rest of the message.
+ * @param {Discord.Client} bot The instantiated Discord Bot object.
  * @param {Discord.message} message An object representing the message sent.
  */
-function processCommand(command, args, message){
+function processCommand(command, args, bot, message){
 
 }
 
