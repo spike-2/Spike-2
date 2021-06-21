2da574be748fbad126c5e3321393ae5cb4bb6866
Brandon Ingli <brandon@brandoningli.com>
2021-06-21 02:26:31 -0500

    Description becomes a function; proivde prefix for help

--- src/blueprintPlugin/main.js
+++ src/blueprintPlugin/main.js
@@ -12,10 +12,6 @@ const spikeKit = require("../spikeKit.js");
  */
 const NAME = "The Name";
 /**
- * The text that will display on the main help screen.
- */
-const DESCRIPTION = "Text that will be on the main help screen";
-/**
  * The author(s) of this plugin.
  */
 const AUTHOR = "Some Person";
@@ -28,12 +24,22 @@ const COMMANDS = ["command1", "command2"];
 
 /**
  * Handles help requests for this plugin.
+ * @param {string} prefix The command prefix.
  * @param {string} command The command issued, without the prefix.
  * @param {string} args The rest of the message.
  * @returns Help text to be sent back to the user.
  */
-function help(command, args) {
+ function help(prefix, command, args) {
   return "How to use the command with given args."
+}
+
+/**
+ * Generates help text for the main help screen.
+ * @param {string} prefix The command prefix.
+ * @returns Help text for the main help screen.
+ */
+ function shortHelp(prefix){
+  return `${prefix}proofofconcept - Just a proof of concept.`
 }
 
 /**
@@ -41,10 +47,10 @@ function help(command, args) {
  * @param {string} command The command issued, without the prefix.
  * @param {string} args The rest of the message.
  * @param {Discord.Client} bot The instantiated Discord Bot object.
- * @param {Discord.message} message An object representing the message sent.
+ * @param {Discord.Message} message An object representing the message sent.
  */
 function processCommand(command, args, bot, message){
 
 }
 
-module.exports = {NAME, DESCRIPTION, AUTHOR, COMMANDS, help, processCommand};
+module.exports = {NAME, shortHelp, AUTHOR, COMMANDS, help, processCommand};
